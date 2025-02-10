5c33d55b90746d64a6dd94a388d150df11626cd9
Mohammad Salim.S.S <77904933+moh4b-z@users.noreply.github.com>
2025-02-09 22:22:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use sticpt'
 
-// import {produtos} from "./produtos.js"
+import {produtos} from "./produtos.js"
 
 const disciplinas = [
     {name: 'PPDM', img: 'icons_facebook.png', color: '#ffc0cb'},
@@ -26,4 +26,44 @@ function criarMenu(disciplina){
 
 }
 
+function criarCards(dados){
+    const listaProdutos = document.getElementById('produtos')
+    const novoCard = document.createElement('div')
+    novoCard.classList.add("card")
+
+    const conteudoCard = document.createElement('div')
+    conteudoCard.classList.add("conteudo")
+    novoCard.appendChild(conteudoCard)
+    const buttonCard = document.createElement('button')
+    novoCard.appendChild(buttonCard)
+
+    const h2Card = document.createElement('h2')
+    conteudoCard.appendChild(h2Card)
+    const spanCard = document.createElement('span')
+    conteudoCard.appendChild(spanCard)
+    const imgCard = document.createElement('img')
+    conteudoCard.appendChild(imgCard)
+    const divCard = document.createElement('div')
+    conteudoCard.appendChild(divCard)
+
+    const spanDivCard = document.createElement('span')
+    divCard.appendChild(spanDivCard)
+    const h4DivCard = document.createElement('h4')
+    divCard.appendChild(h4DivCard)
+    const ulDivCard = document.createElement('ul')
+    divCard.appendChild(ulDivCard)
+
+
+    h2Card.textContent = dados.name
+    spanCard.textContent = dados.description
+    imgCard.src = `./img/${dados.image}`
+    spanDivCard.textContent= `R$: ${dados.price}`
+    novoCard.style = `--cor-card: #${dados.color};`
+    h4DivCard.textContent = 'Size'
+    buttonCard.textContent = 'ADD TO CART'
+
+    listaProdutos.appendChild(novoCard)
+}
+
 disciplinas.forEach(criarMenu)
+produtos.products.forEach(criarCards)
