4179bda5ae400356b07086ad21932fb8c409b6a7
Mohammad Salim.S.S <77904933+moh4b-z@users.noreply.github.com>
2025-02-12 22:02:34 -0300

    Novas funcionalidades no main.js, finalização de algumas coisas anteriomente, agora pode filtra os cards e o size é renderizado

--- main.js
+++ main.js
@@ -3,11 +3,26 @@
 import {produtos} from "./produtos.js"
 
 const disciplinas = [
-    {name: 'PPDM', img: 'icons_facebook.png', color: '#ffc0cb'},
-    {name: 'PRO', img: 'icons_google.png', color: '#ff0000'},
-    {name: 'PWBE', img: 'icons_instagram.png', color: '#0000ff'},
-    {name: 'PWFE', img: 'icons_mail.png', color: '#8a2be2'}
+    {name: 'Touca', img: 'icons_facebook.png', tag: "Touca", color: '#ffffff'},
+    {name: 'Camisa', img: 'icons_google.png', tag: "Camisa", color: '#ffffff'},
+    {name: 'Calça', img: 'icons_instagram.png', tag: "Calca", color: '#ffffff'},
+    {name: 'Tênis', img: 'icons_mail.png', tag: "Tenis", color: '#ffffff'}
 ]
+const sizes = ["XS", "S", "M", "L", "XL"]
+
+function criarSize(tamanhoQueExite, tamanhosQueTem, listaQueVaicolocar) {
+    const ul = listaQueVaicolocar
+
+    tamanhoQueExite.forEach(texto => {
+        const li = document.createElement("li")
+        li.textContent = texto
+        if (tamanhosQueTem.includes(texto)) {
+            li.style.fontWeight = "bold"
+        }
+
+        ul.appendChild(li)
+    })
+}
 
 function criarMenu(disciplina){
     const novoItem = document.createElement('li')
@@ -22,14 +37,42 @@ function criarMenu(disciplina){
     novoItem.appendChild(novoSpan)
     novoItem.style = `--cor-houver: ${disciplina.color}9d;`
 
+    novoItem.addEventListener('click', () => filtrarCards(disciplina.tag))
+
     lista.appendChild(novoItem)
 
 }
+let foiClidao = ""
+function filtrarCards(tag) {
+    const cards = document.querySelectorAll('.card')
 
-function criarCards(dados){
+    if(foiClidao == "" || foiClidao != tag){
+        cards.forEach(card => {
+            const tagCard = card.dataset.tag
+    
+            if (tagCard === tag) {
+                card.style.display = "flex"
+            } else {
+                card.style.display = "none"
+            }
+        })
+        foiClidao = tag
+    }else if(foiClidao == tag){
+        cards.forEach(card => {
+            card.style.display = "flex"  
+        })
+        foiClidao = ""
+    }
+    
+}
+
+
+function criarCards(produto){
     const listaProdutos = document.getElementById('produtos')
     const novoCard = document.createElement('div')
     novoCard.classList.add("card")
+
+    novoCard.dataset.tag = produto.tag
 
     const conteudoCard = document.createElement('div')
     conteudoCard.classList.add("conteudo")
@@ -53,12 +96,14 @@ function criarCards(dados){
     const ulDivCard = document.createElement('ul')
     divCard.appendChild(ulDivCard)
 
+    criarSize(sizes, produto.size, ulDivCard)
 
-    h2Card.textContent = dados.name
-    spanCard.textContent = dados.description
-    imgCard.src = `./img/${dados.image}`
-    spanDivCard.textContent= `R$: ${dados.price}`
-    novoCard.style = `--cor-card: #${dados.color};`
+
+    h2Card.textContent = produto.name
+    spanCard.textContent = produto.description
+    imgCard.src = `./img/${produto.image}`
+    spanDivCard.textContent= `R$: ${produto.price}`
+    novoCard.style = `--cor-card: #${produto.color};`
     h4DivCard.textContent = 'Size'
     buttonCard.textContent = 'ADD TO CART'
 
